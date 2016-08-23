84b4234c154edfe2d42e069ca395c3211ea3d9bc
tzaeschke <zoodb@gmx.de>
2016-08-23 17:06:42 +0200

    Updated kNN test + distance function

--- src/main/java/ch/ethz/globis/phtree/PhDistanceL.java
+++ src/main/java/ch/ethz/globis/phtree/PhDistanceL.java
@@ -42,7 +42,19 @@ public class PhDistanceL implements PhDistance {
   @Override
   public double dist(long[] v1, long[] v2) {
     double d = 0;
+    //How do we best handle this?
+    //Substraction can easily overflow, especially with floating point values that have been 
+    //converted to 'long'.
+    //1) cast to (double). This will lose some precision for large values, but gives a good
+    //   'estimate' and is otherwise quite fault tolerant
+    //2) Use Math.addExact(). This will fall early, and will often not work for converted
+    //   'double' values. However, we can thus enforce using PhDistanceF instead. This
+    //   would be absolutely precise and unlikely to overflow.
+    //The dl*dl can be done as 'double', which is always safe.
     for (int i = 0; i < v1.length; i++) {
+      //TODO replace with Math.exact in Java 8
+      //long dl = Math.subtractExact(v1[i], v2[i]);
+      //d += Math.multiplyExact(dl, dl);
       double dl = (double)v1[i] - (double)v2[i];
       d += dl*dl;
     }
